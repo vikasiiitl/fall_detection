e4c813bb4267fcd087124a2bd37f7300ca72259d
vikasiiitl <167536652+vikasiiitl@users.noreply.github.com>
2024-04-19 19:57:00 +0530

    Add files via upload

---
+++ labels_1.py
@@ -0,0 +1,16 @@
+import os
+import csv
+
+folder_path = '/Users/ajaymaheshwari/Desktop/DEV/AI_Project/Ai-project-5thsem/CNN/Final_1'  # Replace this with the path to your folder
+csv_file = 'labels_1.csv'
+
+def create_label_csv(folder_path, csv_file):
+    with open(csv_file, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['images', 'label'])  # Writing header row
+        for filename in os.listdir(folder_path):
+            print([filename, '0'])
+            csv_writer.writerow([filename, '1'])
+
+create_label_csv(folder_path, csv_file)
+print("Done")
